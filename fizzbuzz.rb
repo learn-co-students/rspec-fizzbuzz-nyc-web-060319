35cedf521c3a225fca72a425be7ef28a9f6044c6
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 02:12:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,23 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+fizz_4 = fizzbuzz(4)
+
+def fizzbuzz
+  if fizz_3 == fizzbuzz(3)
+    puts "Fizz"
+  elsif fizz_5 == fizzbuzz(5)
+    puts "Buzz"
+  elsif fizz_15 = fizzbuzz(15)
+    puts "FizzBuzz"
+  elsif fizz_4 = fizzbuzz(4)
+    puts "nil"
+  end
+end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
